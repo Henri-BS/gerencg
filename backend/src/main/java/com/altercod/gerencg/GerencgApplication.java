ff601dbd9f5c42cf0363900313d7343a11251858
Henrique Santos <rickalmeida720@gmail.com>
2021-12-29 11:35:51 -0300

    Project Created

---
+++ backend/src/main/java/com/altercod/gerencg/GerencgApplication.java
@@ -0,0 +1,13 @@
+package com.altercod.gerencg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerencgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerencgApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/altercod/gerencg/GerencgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altercod.gerencg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerencgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
