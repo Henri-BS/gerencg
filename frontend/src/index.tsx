8b27374b3a146699ee9861c78dc120ccf38a0840
Henrique Santos <rickalmeida720@gmail.com>
2021-12-31 10:44:10 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"; 
+import  ReactDOM  from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/styles.css';
+import App from './App';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<React.StrictMode>
+  <App />
+</React.StrictMode>, document.getElementById('root')
 );
-
 
