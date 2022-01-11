30c98407bc34b54d3764ef24f7d600b3d480c036
Henrique Santos <rickalmeida720@gmail.com>
2022-01-11 07:30:30 -0300

    Pagination

--- backend/src/main/java/com/altercod/gerencg/service/DetailService.java
+++ backend/src/main/java/com/altercod/gerencg/service/DetailService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.altercod.gerencg.dto.DetailDTO;
 import com.altercod.gerencg.entities.Detail;
+import com.altercod.gerencg.repositories.CategoryRepository;
 import com.altercod.gerencg.repositories.DetailRepository;
 
 @Service
@@ -15,8 +17,13 @@ public class DetailService {
 	@Autowired
 	private DetailRepository repository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<DetailDTO> findAll(Pageable pageable){
-			Page<Detail> result = repository.findAll(pageable);
+		categoryRepository.findAll();	
+		Page<Detail> result = repository.findAll(pageable);
 			return result.map(x -> new DetailDTO(x));
 	}
 	
