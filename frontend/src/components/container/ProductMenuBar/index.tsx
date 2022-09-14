6db2209a9c5b05d201916938608315fe2ef0f3b5
Henrique Santos <rickalmeida720@gmail.com>
2022-09-14 20:21:33 -0300

    Delete Product Const Integration

--- frontend/src/components/container/ProductMenuBar/index.tsx
+++ frontend/src/components/container/ProductMenuBar/index.tsx
@@ -2,7 +2,7 @@
 import IUpdateProduct from '../../../assets/img/update-product.png';
 import IDeleteProduct from 'assets/img/delete-product.png';
 import "./styles.css"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { Product } from 'types/product';
 import axios from 'axios';
@@ -13,7 +13,7 @@ productId: string;
 }
 
 function ProductMenuBar({productId}: Props) {
-
+const navigate = useNavigate();
     const [product, setProduct] = useState<Product>();
 
     useEffect(() => {
@@ -23,6 +23,16 @@ function ProductMenuBar({productId}: Props) {
             })
     }, [productId]);
 
+const deleteProduct = () => {
+    
+    
+        axios.delete(`${BASE_URL}/product/delete/${productId}`)
+        .then((response) => {
+            setProduct(response.data);
+            navigate("/product/list")
+        })
+    
+}
     return (
         <div className=" menu-profile-container">
             <Link to={`/product/edit/${productId}`}>
@@ -31,7 +41,7 @@ function ProductMenuBar({productId}: Props) {
                     Editar produto
                 </button>
             </Link>
-            <button className="menu-option-card">
+            <button className="menu-option-card" onClick={() => deleteProduct()}>
                 <img className="option-card-img" src={IDeleteProduct} alt="delete-product" />
                 Deletar produto
             </button>
