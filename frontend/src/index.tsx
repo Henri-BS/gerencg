168a627913ed92c6b1736484fe861224ce4105e5
Henrique Santos <rickalmeida720@gmail.com>
2024-12-13 21:09:54 -0300

    Update files (frontend)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,7 @@ import './assets/css/main.css';
 import './assets/css/card.css';
 import './assets/css/form.css';
 import './assets/css/bar.css';
-import './assets/css/dataTable.css';
+import './assets/css/table.css';
 import './assets/css/page.css';
 
 import App from './App';
